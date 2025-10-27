7e7dbafbdc9d1864b302f03527e8078bf477896b
Vivien Henz <vhenz@college.harvard.edu>
2025-10-27 15:21:18 -0400

    hi

--- src/components/Mission.tsx
+++ src/components/Mission.tsx
@@ -21,9 +21,6 @@ export default function Mission() {
 
         {/* Interactive Information Cards */}
         <div className="mb-8 sm:mb-16">
-          <h2 className="text-xl sm:text-2xl font-normal text-black mb-6 sm:mb-8">
-            {t.aboutNeiomSystems}
-          </h2>
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 sm:gap-6">
             {/* Mission Card */}
             <div className="p-6">
